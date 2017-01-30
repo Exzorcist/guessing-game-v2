b69913b2c49e8c596a1f12e38782cbbba4624315
Exzorcist <dezmant_92@mail.ru>
2017-01-30 16:19:26 +0300

    guessing-game-v2/modife: good work!

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,69 +1,27 @@
 class GuessingGame {
 	
-    constructor(number) {
-    	this.n = number;
-    	//console.log("constructor() this.n - " + this.n);  
+    constructor() {
 
     }
 
     setRange(min, max) {
-    	this.min = min;
-    	this.max = max;
-    	this.rnd = Math.floor(Math.random() * (this.max - this.min)) + this.min;
-    	//console.log("setRange(min, max) " + "max - " + this.max + "  min - " + this.min + " random - " + this.rnd);
+    	this.left = min;
+    	this.right = max;
        }
 
-    guess() {   	
-    	if(this.rnd > this.n){
-    		this.max = this.rnd;
-    		this.rnd = Math.floor(Math.random() * (this.max - this.min)) + this.min;
-    		//console.log("guess() " + "max - " + this.max + "  min - " + this.min + " random - " + this.rnd);
-    		return this.max;
-    	}else if(this.rnd < this.n){
-    		this.min = this.rnd;
-    		this.rnd = Math.floor(Math.random() * (this.max - this.min)) + this.min;
-    		//console.log("guess() " + "max - " + this.max + "  min - " + this.min + " random - " + this.rnd);
-    		return this.min;
-    	}else if(this.rnd == this.n){
-    		this.max = this.rnd;
-    		this.min = this.rnd;
-    		//console.log("guess() " + "max - " + this.max + "  min - " + this.min + " random - " + this.rnd);
-    		return this.rnd;    		
-    	}
-    	
+    guess() {
+    	this.mid = Math.ceil((this.right+this.left)/2);
+    	return this.mid;
     }
 
     lower() {
-    	if((this.rnd==this.max)&&(this.max!=this.n)){
-    		this.max=this.max-1;
-    		//console.log("lower() " + "max - " + this.max + "  min - " + this.min + " random - " + this.rnd);
-    		return this.max;
-    	}else if((this.rnd < this.max)&&(this.rnd > this.n)){
-    		this.max = this.rnd;
-    		this.rnd = Math.floor(Math.random() * (this.max - this.min)) + this.min;
-    		//console.log("lower() " + "max - " + this.max + "  min - " + this.min + " random - " + this.rnd);
-    		return this.max;
-    	}else if(this.rnd < this.n){
-    		this.min = this.rnd;
-    		return this.min;
-    	}
+    	this.right = this.mid;
+    	return this.mid;
     }
 
     greater() {
-    	if((this.rnd==this.min)&&(this.min!=this.n)){
-    		this.min=this.min+1;
-    		//console.log("greater() " + "max - " + this.max + "  min - " + this.min + " random - " + this.rnd);
-    		return this.min;
-    	}else if((this.rnd > this.min)&&(this.rnd < this.n)){
-    		this.min = this.rnd;
-    		this.rnd = Math.floor(Math.random() * (this.max - this.min)) + this.min;
-    		//console.log("greater() " + "max - " + this.max + "  min - " + this.min + " random - " + this.rnd);
-    		return this.min;
-    	}else if(this.rnd > this.n){
-    		this.max = this.rnd;
-    		return this.max;
-    	}
-
+    	this.left = this.mid;
+    	return this.mid;
     }
 }
 
